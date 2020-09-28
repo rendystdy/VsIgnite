6a30456a71af5937d40da99cee4ed843226a4699
Rendy Setiadi <rendysetiady8@gmail.com>
2020-09-28 18:39:28 +0700

    Feat: add codepush Ios & Android

--- android/app/src/main/java/com/vsignite/MainApplication.java
+++ android/app/src/main/java/com/vsignite/MainApplication.java
@@ -1,6 +1,6 @@
 package com.vsignite;
 
-import android.app.Application;
+import androidx.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -15,17 +15,22 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
-
+import com.microsoft.codepush.react.CodePush;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = 
     new ReactNativeHost(this) {
       @Override
       public boolean getUseDeveloperSupport() {
         return BuildConfig.DEBUG;
+      }
+
+      @Override
+      protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
       }
 
       @Override
